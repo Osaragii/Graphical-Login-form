d03481a37fc6fbd9e79c9ebff8f2ec9b0dcadade
Osaragii <dalaidev9@gmail.com>
2024-12-02 13:37:39 +0530

    Created a JFrame

---
+++ LoginForm.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//Created a JFrame
+//Created username and password labels and text fields
+//Created a submit button
+//Created a label to display messages
+
+public class LoginForm {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Login Form");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 200);
+        frame.setLayout(new GridLayout(4, 1));
+
+        JLabel usernameLabel = new JLabel("Username:", SwingConstants.CENTER);
+        JTextField usernameField = new JTextField();
+        JLabel passwordLabel = new JLabel("Password:", SwingConstants.CENTER);
+        JPasswordField passwordField = new JPasswordField();
+
+        JButton submitButton = new JButton("Submit");
+
+        JLabel messageLabel = new JLabel("", SwingConstants.CENTER);
+
+    }
+}
